f251cb0bf4f1e1c82718a49d10a4da2c5eeeb737
RaghuR01 <bheemesh.halmark@gmail.com>
2024-01-10 10:35:22 +0530

    This is the commit from windows

--- Demo.java
+++ Demo.java
@@ -3,5 +3,9 @@ class Demo
   public static void main(String args[])
   {
     System.out.println("Hello From GitHub");
+    for(int i=1;i<=5;i++)
+    {
+	System.out.println("Hi from Windows");
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CiscoTraining1
 This is for the training purpose
+Today is 3rd day of training
